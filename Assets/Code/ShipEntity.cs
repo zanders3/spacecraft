de05ccb108bba04c395d6dc229b02b55caa667dd
zanders3 <3zanders@gmail.com>
2013-07-01 20:51:16 +0100

    Added solar system loading and saving.

--- Assets/Code/ShipEntity.cs
+++ Assets/Code/ShipEntity.cs
@@ -23,15 +23,6 @@ public class ShipEntity : Entity
         SetBlock(BlockType.PowerCore, Point3D.Zero);
     }
 
-    public static void CreateShip(Material material, Vector3 pos, Vector3 up)
-    {
-        ShipEntity ship = new GameObject("Ship").AddComponent<ShipEntity>();
-        ship.gameObject.AddComponent<Rigidbody>();
-        ship.Material = material;
-        ship.transform.position = pos;
-        ship.transform.rotation = Quaternion.LookRotation(up) * Quaternion.AngleAxis(90.0f, Vector3.right);
-    }
-
     protected override void OnChunksUpdated()
     {
         int mass = chunkStore.Mass;
